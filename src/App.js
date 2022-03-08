5c26d9854b8a7289f991694ec321afa868cb8a8e
mahmudul <bappy392@gmail.com>
2022-03-08 15:47:13 +0600

    react bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,154 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState }  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// const info={
+//   firstName:'',
+// }
 
 function App() {
+
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName:'',
+    age:'',
+    profession:'',
+    gender:'male',
+  })
+
+  const [errorInfo, setErrorInfo]=useState({
+    firstName: '',
+    lastName:'',
+    age:''
+  })
+
+  const handleChange=(evt)=>{
+      setUserInfo({
+        ...userInfo,
+        [evt.target.name]:evt.target.value
+
+      })
+
+      setErrorInfo('')
+    
+  }
+
+  const handleSubmit=(evt)=>{
+    evt.preventDefault()
+
+    //validation
+    const {firstName,lastName,age,profession,gender}=userInfo
+     
+    // if(firstName===''|| lastName===''|| age===''){
+    //   setErrorInfo('Please Provide Ncessary Information')
+    //   return
+    // }
+    if(firstName===''){
+      setErrorInfo({
+        ...errorInfo,
+        firstName:'Please provide FirstName'
+      })
+      return
+    }
+
+    console.log('Submitting')
+
+    console.log(userInfo)
+
+      //reset
+  setUserInfo({
+    firstName: '',
+    lastName:'',
+    age:'',
+    profession:'',
+    gender:'male',
+  })
+
+
+  }
+
+
+
+  const {firstName,lastName,age,profession,gender}=userInfo
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {/* {errorInfo && errorInfo} */}
+
+     <form onSubmit={handleSubmit}>
+        <h1>Forms with React</h1>
+         
+         <div className='form-group'>
+           {errorInfo.firstName && errorInfo.firstName}
+           <label htmlFor='firstName'>First Name:</label>
+           <input type='text'
+                name='firstName'
+                id='firstName'
+                value={firstName}
+                onChange={handleChange}
+              />
+         </div>
+
+         <div className='form-group'>
+           <label htmlFor='lastName'>Last Name:</label>
+           <input type='text'
+                name='lastName'
+                id='lastName'
+                value={lastName}
+                onChange={handleChange}
+              />
+         </div>
+
+
+         <div className='form-group'>
+           <label htmlFor='age'>Age Name:</label>
+           <input type='number'
+                name='age'
+                id='age'
+                value={age}
+                onChange={handleChange}
+              />
+         </div>
+         
+         <div className='form-group'>
+           <label htmlFor='profession'>Profession:</label>
+           <select
+             id='profession'
+             name='profession'
+             onChange={handleChange}
+             value={profession}
+
+           >
+             <option value='developer'>Developer</option>
+             <option value='designer'>Designer</option>
+             <option value='programmer'>Programmer</option>
+           </select>
+
+         </div>
+
+         <div>
+           <label htmlFor='gender'>Gender:</label>
+           Male <input 
+               name='gender'
+               type='radio'
+               value='male'
+               checked={gender==='male'}
+               onChange={handleChange}      
+            />
+           Female <input
+               name='gender'
+                type='radio'
+                value='female'  
+                checked={gender==='female'}
+                onChange={handleChange}
+           />
+         </div>
+          
+          <input type='submit' value='Submit'/>
+     
+     </form>
+    </>
   );
 }
 
